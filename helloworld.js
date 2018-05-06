88725ee6f499fd42910c1ec0c55d71d5652b3913
Gary Wilber <Spacetech326@gmail.com>
2018-05-06 10:52:07 -0700

    Development branch update

--- helloworld.js
+++ helloworld.js
@@ -1,38 +1,55 @@
-define(["require", "exports", "Enums", "item/Items", "language/Messages", "mod/Mod", "tile/Terrains", "Utilities"], function (require, exports, Enums_1, Items_1, Messages_1, Mod_1, Terrains_1, Utilities) {
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+define(["require", "exports", "Enums", "item/Items", "language/IMessages", "mod/Mod", "tile/Terrains", "utilities/TileHelpers"], function (require, exports, Enums_1, Items_1, IMessages_1, Mod_1, Terrains_1, TileHelpers_1) {
     "use strict";
     Object.defineProperty(exports, "__esModule", { value: true });
+    let log;
     class HelloWorld extends Mod_1.default {
         onInitialize() {
             this.dictionary = this.addDictionary("HelloWorld", HelloWorldDictionary);
+            log = this.getLog();
         }
         onLoad(saveData) {
-            console.log("Hello World!");
+            log.info("Hello World!");
         }
         onUnload() {
-            console.log("Goodbye World!");
+            log.info("Goodbye World!");
         }
         onGameStart(isLoadingSave, playedCount) {
-            ui.displayMessage(localPlayer, languageManager.getTranslationString(this.dictionary, HelloWorldDictionary.HelloWorld), Messages_1.MessageType.Good);
+            ui.displayMessage(localPlayer, languageManager.getTranslationString(this.dictionary, HelloWorldDictionary.HelloWorld), IMessages_1.MessageType.Good);
             Items_1.default[Enums_1.ItemType.Branch].prefix = "a ";
             Items_1.default[Enums_1.ItemType.Branch].name = "greetings stick";
         }
         onItemEquip(player, item, slot) {
             if (item.type === Enums_1.ItemType.Branch) {
                 if (slot === Enums_1.EquipType.LeftHand) {
-                    ui.displayMessage(localPlayer, languageManager.getTranslationString(this.dictionary, HelloWorldDictionary.HelloLeftHand), Messages_1.MessageType.None);
+                    ui.displayMessage(localPlayer, languageManager.getTranslationString(this.dictionary, HelloWorldDictionary.HelloLeftHand), IMessages_1.MessageType.None);
                 }
                 else {
-                    ui.displayMessage(localPlayer, languageManager.getTranslationString(this.dictionary, HelloWorldDictionary.HelloRightHand), Messages_1.MessageType.None);
+                    ui.displayMessage(localPlayer, languageManager.getTranslationString(this.dictionary, HelloWorldDictionary.HelloRightHand), IMessages_1.MessageType.None);
                 }
             }
         }
         onMove(player, nextX, nextY, tile, direction) {
             const getTile = game.getTile(localPlayer.x, localPlayer.y, localPlayer.z);
-            const tileType = Utilities.TileHelpers.getType(getTile);
-            ui.displayMessage(localPlayer, languageManager.getTranslationString(this.dictionary, HelloWorldDictionary.HelloTerrain), Messages_1.MessageType.Stat, Terrains_1.default[tileType].name);
+            const tileType = TileHelpers_1.default.getType(getTile);
+            ui.displayMessage(localPlayer, languageManager.getTranslationString(this.dictionary, HelloWorldDictionary.HelloTerrain), IMessages_1.MessageType.Stat, Terrains_1.default[tileType].name);
             return undefined;
         }
     }
+    __decorate([
+        Mod_1.HookMethod
+    ], HelloWorld.prototype, "onGameStart", null);
+    __decorate([
+        Mod_1.HookMethod
+    ], HelloWorld.prototype, "onItemEquip", null);
+    __decorate([
+        Mod_1.HookMethod
+    ], HelloWorld.prototype, "onMove", null);
     exports.default = HelloWorld;
     var HelloWorldDictionary;
     (function (HelloWorldDictionary) {
--- helloworld.ts
+++ helloworld.ts
@@ -1,49 +1,57 @@
 import { EquipType, FacingDirection, ItemType } from "Enums";
 import { IItem } from "item/IItem";
 import Items from "item/Items";
-import { MessageType } from "language/Messages";
-import Mod from "mod/Mod";
+import { MessageType } from "language/IMessages";
+import Mod, { HookMethod } from "mod/Mod";
 import { IPlayer } from "player/IPlayer";
 import { ITile } from "tile/ITerrain";
 import Terrains from "tile/Terrains";
-import * as Utilities from "Utilities";
+import Log, { Source } from "utilities/Log";
+import TileHelpers from "utilities/TileHelpers";
+
+let log: Log;
 
 export default class HelloWorld extends Mod {
 	private dictionary: number;
 
 	public onInitialize(): void {
 		this.dictionary = this.addDictionary("HelloWorld", HelloWorldDictionary);
+		log = this.getLog();
 	}
 
 	public onLoad(saveData: any): void {
-		console.log("Hello World!");
+		log.info("Hello World!");
 	}
 
 	public onUnload(): void {
-		console.log("Goodbye World!");
+		log.info("Goodbye World!");
 	}
 
 	// Hooks
+
+	@HookMethod
 	public onGameStart(isLoadingSave: boolean, playedCount: number): void {
 		ui.displayMessage(localPlayer, languageManager.getTranslationString(this.dictionary, HelloWorldDictionary.HelloWorld), MessageType.Good);
 		Items[ItemType.Branch].prefix = "a ";
 		Items[ItemType.Branch].name = "greetings stick";
 	}
 
+	@HookMethod
 	public onItemEquip(player: IPlayer, item: IItem, slot: EquipType): void {
 		if (item.type === ItemType.Branch) {
 			if (slot === EquipType.LeftHand) {
 				ui.displayMessage(localPlayer, languageManager.getTranslationString(this.dictionary, HelloWorldDictionary.HelloLeftHand), MessageType.None);
-				
+
 			} else {
 				ui.displayMessage(localPlayer, languageManager.getTranslationString(this.dictionary, HelloWorldDictionary.HelloRightHand), MessageType.None);
 			}
 		}
 	}
 
+	@HookMethod
 	public onMove(player: IPlayer, nextX: number, nextY: number, tile: ITile, direction: FacingDirection): boolean | undefined {
 		const getTile = game.getTile(localPlayer.x, localPlayer.y, localPlayer.z);
-		const tileType = Utilities.TileHelpers.getType(getTile);
+		const tileType = TileHelpers.getType(getTile);
 		ui.displayMessage(localPlayer, languageManager.getTranslationString(this.dictionary, HelloWorldDictionary.HelloTerrain), MessageType.Stat, Terrains[tileType].name);
 		return undefined;
 	}
